e2800cb8884d8d0181b840a36f2f3f3f5cbe688e
Ganaga <ganaga@laposte.net>
2023-05-15 13:32:52 +0200

    changes from ggobo42sp7

--- index_win.js
+++ index_win.js
@@ -26,21 +26,11 @@ app.get("/puppeteer", async (req, res) => {
   const height = req.query.height;
   const page = await browser.newPage();
   // to add cookie
-  
+  console.log(url);
   const cookies = req.cookies;
   const objCoos = cookieParser.JSONCookies(cookies);
-  console.log(objCoos.GaligeoToken);
-  
-   let urlLogin = url.substring(0, url.indexOf('viewer/index.html')) + 'feature/portal/login';
-   console.log(urlLogin);
-   let token = objCoos.GaligeoToken;
-   if(!objCoos.GaligeoToken || objCoos.GaligeoToken === 'null') {
-    let responseLogin = await axios.post(urlLogin, 'userName=Administrator&password=WG0dem0');
-    token = responseLogin.data.token;
-	console.log('l40', token);
-   }
-   
-   console.log(token);
+  let token = objCoos.GaligeoToken;
+  console.log(token);
   const cos = [
     {
       name: "GaligeoToken",
@@ -49,9 +39,10 @@ app.get("/puppeteer", async (req, res) => {
       path: "/",
     },
   ];
-await page.setCookie(...cos);
+  await page.setCookie(...cos);
 
-  await page.setViewport({ width: 1080, height:  1024 });
+
+  await page.setViewport({ width: 1080, height: 1024 });
   console.log('setViewport');
   await page.goto(
     url,
@@ -60,33 +51,35 @@ await page.setCookie(...cos);
     }
   );
   console.log('waitForResponse');
-  const first =  Date.now();
+  const first = Date.now();
   let imgIsCached = false;
   await page.waitForResponse(
     async (response) => {
-      console.log(response.url());
-      if((first + 100000) <  Date.now()  ) {
-        console.log('force exit afer 100000 ms');
+      //console.log(response.url());
+      if ((first + 5000) < Date.now()) {
+        console.log('force exit afer 40000 ms');
         return true;
       }
-      if(response.url().includes("cache")) {
+      if (response.url().includes("cache")) {
         imgIsCached = true;
       }
-      return  response.url().includes("cache");
+      return response.url().includes("cache");
     },
     { timeout: 0 }
   );
+
 
   // Set screen size
   console.log('close');
   await browser.close();
   res.set({ "Content-Type": "application/json" });
-  if(imgIsCached) {
+  if (imgIsCached) {
     res.send({ message: "Image is in cache folder" });
-  }else {
+  } else {
     res.send({ message: "time out" });
   }
-  
+  //res.send({ message: "Image is in cache folder" });
+
 });
 
 app.listen(port, () => {
