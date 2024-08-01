e42b982a534b5bfd99e034200f5029f4e4b0631d
jmgaudin <88384553+jmgaudin@users.noreply.github.com>
2024-08-01 11:47:35 +0200

    index_win - code format

--- index_win.js
+++ index_win.js
@@ -27,9 +27,9 @@ app.get("/print", async (req, res) => {
     console.log('start call');
     const url = decodeURIComponent(req.query.url);
     console.log(req.query);
-    const width = parseInt(req.query.width) ;
-    const height =  parseInt(req.query.height);
-    const printTimeOut =  parseInt(req.query.print_timeout) ? parseInt(req.query.print_timeout) : 60000;
+    const width = parseInt(req.query.width);
+    const height = parseInt(req.query.height);
+    const printTimeOut = parseInt(req.query.print_timeout) ? parseInt(req.query.print_timeout) : 60000;
     const page = await browser.newPage();
     // to add cookie
     console.log(url);
@@ -46,9 +46,9 @@ app.get("/print", async (req, res) => {
       },
     ];
     await page.setCookie(...cos);
-  
-  
-    await page.setViewport({ width: width ? width : 1080, height: height? height :1024 });
+
+
+    await page.setViewport({ width: width ? width : 1080, height: height ? height : 1024 });
     console.log('setViewport');
     const { blue, cyan, green, magenta, red, yellow } = require('colorette')
     let imgIsCached = false;
@@ -65,10 +65,10 @@ app.get("/print", async (req, res) => {
         console.log(color(`${type} ${message.text()}`))
       })
       .on('pageerror', ({ message }) => console.log(red(message)))
-      .on('response', response =>{
+      .on('response', response => {
         console.log(green(`${response.status()} ${response.url()}`))
 
-        if (response.url().includes("cache") && response.status()  === 200) {
+        if (response.url().includes("cache") && response.status() === 200) {
           imgIsCached = true;
           console.log("Image is cached BEFORE waitForResponse(...)")
         }
@@ -77,7 +77,7 @@ app.get("/print", async (req, res) => {
       )
       .on('requestfailed', request =>
         console.log(magenta(`${request.failure().errorText} ${request.url()}`)))
-       
+
     await page.goto(
       url,
       {
@@ -86,48 +86,48 @@ app.get("/print", async (req, res) => {
     );
     console.log('waitForResponse >>');
     const first = Date.now();
-    
+
     await page.waitForResponse(
       async (response) => {
         //console.log('waitForResponse - response.url(): '+response.url());
-        
+
         if (imgIsCached) {
           console.log("waitForResponse - Image is ALREADY cached")
           return true;
         }
-        
-        if (response.url().includes("cache") && response.status()  === 200) {
+
+        if (response.url().includes("cache") && response.status() === 200) {
           imgIsCached = true;
           console.log("waitForResponse - Image is cached INSIDE waitForResponse")
           return true;
         }
 
         if ((first + printTimeOut) < Date.now()) {
-          console.log('waitForResponse - force exit afer '+printTimeOut+' ms');
+          console.log('waitForResponse - force exit afer ' + printTimeOut + ' ms');
           return true;
         }
-        
+
         return false;
       },
       { timeout: 0 }
     );
-  
-  
+
+
     // Set screen size
     console.log('close');
     await browser.close();
     res.set({ "Content-Type": "application/json" });
     if (imgIsCached) {
       res.send({ message: "Image is in cache folder" });
-	  console.log("Image is in cache folder");
+      console.log("Image is in cache folder");
     } else {
-      res.send({ message: 'time out after '+printTimeOut+' ms'});
-	  console.log('time out after '+printTimeOut+' ms');
+      res.send({ message: 'time out after ' + printTimeOut + ' ms' });
+      console.log('time out after ' + printTimeOut + ' ms');
     }
   } catch (error) {
-    res.send({ log:error,message: 'print server can t print image' });
-	console.log('print server can t print image');
-	console.log('error:',error);
+    res.send({ log: error, message: 'print server can t print image' });
+    console.log('print server can t print image');
+    console.log('error:', error);
   }
 
 });
