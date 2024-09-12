212dfb391a7da18d414148a3fce2f5e16f10a5ff
jmgaudin <88384553+jmgaudin@users.noreply.github.com>
2024-09-12 10:04:25 +0200

    index_win - added log-timestamp

--- index_win.js
+++ index_win.js
@@ -1,4 +1,3 @@
-
 const puppeteer = require("puppeteer");
 const config = require('./index_win_config'); // Import the configuration file
 const express = require("express");
@@ -7,6 +6,23 @@ const cookieParser = require("cookie-parser");
 
 const axios = require("axios");
 app.use(cookieParser());
+
+const logTimeStamp = require('log-timestamp');
+logTimeStamp (function() { 
+  return '[' + toLocalISOString(new Date()) + '] - %s' 
+});
+
+function toLocalISOString(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Les mois commencent à 0
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
 
 
 const port = process.env.GGO_PRINT_HTTP_PORT || config.http_port || 3000;
